4d47d4b3c5be2e46139843450a9ec54069fc66ab
Molledk <MortenBaagoee@hotmail.com>
2015-05-17 19:34:41 +0200

    så er der lidt server værk ....

---
+++ src/com/scrom/server/Lobby.java
@@ -0,0 +1,100 @@
+package server;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.HashSet;
+
+
+public class Lobby {
+
+    /**
+     * The port that the server listens on.
+     */
+    int PORT;
+
+
+    private static HashSet<String> names = new HashSet<String>();
+
+
+    private static HashSet<PrintWriter> writers = new HashSet<PrintWriter>();
+
+
+    /**
+     * A handler thread class.  Handlers are spawned from the listening
+     * loop and are responsible for a dealing with a single client
+     * and broadcasting its messages.
+     */
+    private static class Handler extends Thread {
+        private String name;
+        private Socket socket;
+        private BufferedReader in;
+        private PrintWriter out;
+
+        /**
+         * Constructs a handler thread, squirreling away the socket.
+         * All the interesting work is done in the run method.
+         */
+        public Handler(Socket socket) {
+            this.socket = socket;
+        }
+
+
+        public void run() {
+            try {
+
+                in = new BufferedReader(new InputStreamReader(
+                        socket.getInputStream()));
+                out = new PrintWriter(socket.getOutputStream(), true);
+
+
+                while (true) {
+                    out.println("SUBMITNAME");
+                    name = in.readLine();
+                    if (name == null) {
+                        return;
+                    }
+                    synchronized (names) {
+                        if (!names.contains(name)) {
+                            names.add(name);
+                            break;
+                        }
+                    }
+                }
+
+
+                out.println("NAMEACCEPTED");
+                out.println("MESSAGE Welcome to: "+name);
+                writers.add(out);
+
+
+                while (true) {
+                    String input = in.readLine();
+                    if (input == null) {
+                        return;
+                    }
+                    for (PrintWriter writer : writers) {
+                        writer.println("MESSAGE " + name + ": " + input);
+                    }
+                }
+            } catch (IOException e) {
+                System.out.println(e);
+            } finally {
+
+                if (name != null) {
+                    names.remove(name);
+                }
+                if (out != null) {
+                    writers.remove(out);
+                }
+                try {
+                    socket.close();
+                } catch (IOException e) {
+                }
+            }
+        }
+    }
+}
---
+++ src/com/scrom/server/Server.java
@@ -0,0 +1,7 @@
+package com.scrom.server;
+
+/**
+ * Created by Mortenmus on 17-05-2015.
+ */
+public class Server {
+}
